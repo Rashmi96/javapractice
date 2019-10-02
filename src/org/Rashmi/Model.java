6f262891aaf06d5e9347e8a7b65e003f38aa0531
Rashmi <swain96@gmail.com>
2019-10-02 10:55:05 +0530

    added getter-setter

--- src/org/Rashmi/Model.java
+++ src/org/Rashmi/Model.java
@@ -3,4 +3,12 @@ package org.Rashmi;
 public class Model {
     private String name;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
